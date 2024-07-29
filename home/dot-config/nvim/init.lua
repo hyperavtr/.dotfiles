cf8ddb06b09d81df97c67b2e3fbdea2a4ebc8d4b
avtr <hyperavtr@skiff.com>
2024-07-29 08:21:13 +0300

    Full backup

--- home/dot-config/nvim/init.lua
+++ home/dot-config/nvim/init.lua
@@ -184,20 +184,16 @@ require("lazy").setup({
 			require("which-key").setup()
 
 			-- Document existing key chains
-			require("which-key").register({
-				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-				["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-				["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
-				["<leader>R"] = { name = "[R]un", _ = "which_key_ignore" },
+			require("which-key").add({
+				{ "<leader>R", group = "[R]un" },
+				{ "<leader>c", group = "[C]ode" },
+				{ "<leader>d", group = "[D]ocument" },
+				{ "<leader>r", group = "[R]ename" },
+				{ "<leader>s", group = "[S]earch" },
+				{ "<leader>t", group = "[T]oggle" },
+				{ "<leader>w", group = "[W]orkspace" },
+				{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
 			})
-			-- visual mode
-			require("which-key").register({
-				["<leader>h"] = { "Git [H]unk" },
-			}, { mode = "v" })
 		end,
 	},
 
@@ -501,7 +497,7 @@ require("lazy").setup({
 				--    https://github.com/pmizio/typescript-tools.nvim
 				--
 				-- But for many setups, the LSP (`tsserver`) will work just fine
-				-- tsserver = {},
+				tsserver = {},
 				--
 
 				lua_ls = {
@@ -532,13 +528,12 @@ require("lazy").setup({
 			-- for you, so that they are available from within Neovim.
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
-				"stylua", -- Used to LSP Lua
-				"bashls", --Used to LSP Bash
-				"basedpyright", --Used to LSP Python
-				"clangd", --Used to LSP C
-				"html", --Used to LSP HTML markdown
-				"cssls", --Used to LSP CSS
-				"eslint", --Used to JS(TS)
+				"stylua", -- LSP Lua
+				"bashls", --LSP Bash
+				"basedpyright", --LSP Python
+				"clangd", --LSP C
+				"html", -- LSP HTML markdown
+				"cssls", --LSP CSS
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
@@ -589,8 +584,8 @@ require("lazy").setup({
 				--
 				-- You can use a sub-list to tell conform to run *until* a formatter
 				-- is found.
-				javascript = { "prettierd" },
-				typescript = { "prettierd" },
+				--javascript = { "prettierd" },
+				--typescript = { "prettierd" },
 				--html = { "prettierd" },
 				--css = { "prettierd" },
 				bash = { "beautysh" },
